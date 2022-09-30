9d5637952856a8797418bcd918c920436b07bcea
drnimbusrain <dr.nimbusrain@gmail.com>
2022-09-30 13:25:52 -0400

    Quick merge before pull.

--- canopy_phot_mod.F90
+++ canopy_phot_mod.F90
@@ -5,7 +5,7 @@ module canopy_phot_mod
 contains
 
 !:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-    SUBROUTINE CANOPY_PHOT( HCM, ZK, FCLAI, LAI, CLU, COSZEN, RJCORR )
+    SUBROUTINE CANOPY_PHOT( HC, ZK, FCLAI, LAI, CLU, COSZEN, RJCORR )
 
 !-----------------------------------------------------------------------
 
@@ -27,7 +27,7 @@ contains
 
 ! Arguments:
 !     IN/OUT
-        REAL(RK),    INTENT( IN )  :: HCM             ! Height of canopy top (m)
+        REAL(RK),    INTENT( IN )  :: HC              ! Height of canopy top (m)
         REAL(RK),    INTENT( IN )  :: ZK              ! Above/Below canopy height, z (m)
         REAL(RK),    INTENT( IN )  :: FCLAI           ! Model input Fractional (z) shapes of the
         ! plant surface distribution (nondimensional), i.e., a Fractional Culmulative LAI
@@ -42,7 +42,7 @@ contains
 !Makar, P., Staebler, R., Akingunola, A. et al. The effects of forest canopy shading and turbulence on boundary layer ozone.
 !Nat Commun 8, 15243 (2017). https://doi.org/10.1038/ncomms15243
 
-        if (ZK <= HCM) then       !at or below canopy top --> calculate photolysis attenuation
+        if (ZK <= HC) then       !at or below canopy top --> calculate photolysis attenuation
             RJCORR = MAX(1.0E-10_rk, EXP(-1.0_rk*(0.5_rk*(LAI*(1.0_rk-FCLAI))*CLU)/MAX(0.05_rk, COSZEN)))
         else
             RJCORR = 1.0_rk       !above canopy top RJCORR = 1
