2e73f7d57c4cf5e0f77e0cf754819291667b9e49
Wei-Ting Hung <107704243+angehung5@users.noreply.github.com>
2023-09-28 20:39:19 -0400

    Update canopy_readnml.F90

--- src/canopy_readnml.F90
+++ src/canopy_readnml.F90
@@ -22,7 +22,7 @@ SUBROUTINE canopy_readnml
 
     NAMELIST /userdefs/  infmt_opt, time_start, time_end, time_intvl, ntime, &
         nlat, nlon, modlays, modres, href_opt, href_set, z0ghc, lambdars, &
-        flameh_opt, flameh_frp, flameh_set, frp_fac, ifcanwind, &
+        flameh_opt, flameh_cal, flameh_set, frp_fac, ifcanwind, &
         ifcanwaf, ifcaneddy, ifcanphot, ifcanbio, pai_opt, pai_set, lu_opt, z0_opt, &
         dx_opt, dx_set, lai_thresh, frt_thresh, fch_thresh, rsl_opt, bio_cce, &
         biovert_opt, ssg_opt, ssg_set, crop_opt, crop_set, co2_opt, co2_set
@@ -123,7 +123,7 @@ SUBROUTINE canopy_readnml
 
 !-------------------------------------------------------------------------------
 ! Set default integer for FRP to flame height relationships used (default = 0)
-    flameh_frp = 0
+    flameh_cal = 0
 !-------------------------------------------------------------------------------
 
 !-------------------------------------------------------------------------------
