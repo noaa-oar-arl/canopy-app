733b8d2d56cbb50983b199ee9fa41bf58f8c6622
drnimbusrain <dr.nimbusrain@gmail.com>
2023-12-07 20:55:33 -0500

    Initial changes to PAVD for shape distribution conversion.

--- src/canopy_var3din_mod.F90
+++ src/canopy_var3din_mod.F90
@@ -41,6 +41,15 @@ contains
         REAL(RK)                    :: PAI                  ! Plant Area Index, m2/m2
         REAL(RK)                    :: ZK(SIZE(ZHC))
         REAL(RK)                    :: PAVD_INTERP(SIZE(ZHC))
+
+        REAL(RK)                    :: ZCANMAX               ! Height of maximum foliage area density (z/h) (nondimensional)
+        REAL(RK)                    :: PAVD_BELOW            ! Average of PAVD below zcanmax
+        REAL(RK)                    :: PAVD_ABOVE            ! Average of PAVD above zcanmax
+        REAL(RK)                    :: SIGMAU                ! Standard deviation of shape function above zcanmax (z/h)
+        REAL(RK)                    :: SIGMA1                ! Standard deviation of shape function below zcanmax (z/h)
+
+
+
 !        REAL(RK), allocatable       :: fainc(:)              ! incremental foliage shape function
 !        REAL(RK), allocatable       :: fafracz(:)            ! incremental fractional foliage shape function
 !        REAL(RK)                    :: fatot                 ! integral of total fractional foliage shape function
@@ -51,17 +60,30 @@ contains
         PAVD_INTERP = 0.0_rk  !Initialize PAVD_INTERP = 0
         do lev=1, SIZE(PAVD_LEVS) - 1
             do i=2, SIZE(ZK)  !loop over only levels ABOVE ground
-                if (ZK(i) .le. FCH) then !constrain to less than the forest canopy height observed (e.g., GEDI CH)
-                    if (ZK(i) .le.  PAVD_LEVS(1)) then
-                        PAVD_INTERP(i)   = PAVD_IN(1)
-                    end if
-                    if (ZK(i) .ge.  PAVD_LEVS(lev) .and. ZK(i) .le.  PAVD_LEVS(lev+1)) then
-                        PAVD_INTERP(i)   = interp_linear1_internal((/ PAVD_LEVS(lev),PAVD_LEVS(lev+1) /), &
-                            (/ PAVD_IN(lev),PAVD_IN(lev+1) /),ZK(i))
-                    end if
+!                if (ZK(i) .le. FCH) then !constrain to less than the forest canopy height observed (e.g., GEDI CH)
+                if (ZK(i) .le.  PAVD_LEVS(1)) then
+                    PAVD_INTERP(i)   = PAVD_IN(1)
                 end if
+                if (ZK(i) .ge.  PAVD_LEVS(lev) .and. ZK(i) .le.  PAVD_LEVS(lev+1)) then
+                    PAVD_INTERP(i)   = interp_linear1_internal((/ PAVD_LEVS(lev),PAVD_LEVS(lev+1) /), &
+                        (/ PAVD_IN(lev),PAVD_IN(lev+1) /),ZK(i))
+                end if
+!                end if
             end do
         end do
+
+!bottom-up loop allowing ZCANMAX, SIGMAU, and SIGMA1 calculation from observed PAVD
+        do i=2, SIZE(ZK)
+            if (PAVD_INTERP(i) .ge. maxval(PAVD_INTERP) ) then
+                ZCANMAX = ZK(i)/FCH
+                PAVD_BELOW = SUM(PAVD_INTERP(2:i)) / i
+                SIGMA1 = (SQRT (SUM((PAVD_INTERP(2:i)-PAVD_BELOW)**2) / i))/FCH
+                PAVD_ABOVE = SUM(PAVD_INTERP(i:SIZE(ZK))) / (SIZE(ZK)-i)
+                SIGMAU = (SQRT (SUM((PAVD_INTERP(i:SIZE(ZK))-PAVD_ABOVE)**2) / (SIZE(ZK)-i)))/FCH
+                exit
+            end if
+        end do
+        print*, ZCANMAX,PAVD_BELOW,SIGMA1,PAVD_ABOVE,SIGMAU
 
 !Integrate the PAVD_INTERP to get the total PAI
         PAI = IntegrateTrapezoid(ZK,PAVD_INTERP)
