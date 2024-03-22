04664f3ae8bc5db4b8fb2604a7c0441194534e98
drnimbusrain <dr.nimbusrain@gmail.com>
2024-03-22 13:11:13 -0400

    Updated NCF and TXT output file name to use valid time stamp.

--- src/canopy_app.F90
+++ src/canopy_app.F90
@@ -20,8 +20,10 @@ program canopy_app
 
     CHARACTER(LEN=24)                 :: time_next  ! YYYY-MM-DD-HH:MM:SS.SSSS
     CHARACTER(LEN=24)                 :: time_now   ! YYYY-MM-DD-HH:MM:SS.SSSS
-    integer   :: nn
-    CHARACTER(LEN=24)                 :: nn_string
+    CHARACTER(LEN=24)                 :: time_now_file
+
+    integer   :: n,nn
+!    CHARACTER(LEN=24)                 :: nn_string
 !-------------------------------------------------------------------------------
 ! Error, warning, and informational messages.
 !-------------------------------------------------------------------------------
@@ -92,29 +94,40 @@ program canopy_app
 !-------------------------------------------------------------------------------
 ! Write model output of canopy model calculations.
 !-------------------------------------------------------------------------------
-        write(nn_string,*) nn-1
-!        call canopy_write_txt(trim(file_out(1)) // '_' // trim(time_now))
-        if (nn.lt.10) then
-            call canopy_write_txt((trim(file_out(1)) // '_t00' // ADJUSTL(nn_string)), &
-                time_now)
-        else if (nn.ge.10.and.nn.lt.100) then
-            call canopy_write_txt((trim(file_out(1)) // '_t0' // ADJUSTL(nn_string)), &
-                time_now)
-        else
-            call canopy_write_txt((trim(file_out(1)) // '_t' // ADJUSTL(nn_string)), &
-                time_now)
-        end if
+!        write(nn_string,*) nn-1
+
+        time_now_file=time_now
+        do n = 1, len(time_now_file)
+            if (time_now_file(n:n) == ':') then
+                time_now_file(n:n) = '-'
+            end if
+        end do
+
+        call canopy_write_txt((trim(file_out(1)) // '_' // trim(time_now_file)),time_now)
+
+!        if (nn.lt.10) then
+!            call canopy_write_txt((trim(file_out(1)) // '_t00' // ADJUSTL(nn_string)), &
+!                time_now)
+!        else if (nn.ge.10.and.nn.lt.100) then
+!            call canopy_write_txt((trim(file_out(1)) // '_t0' // ADJUSTL(nn_string)), &
+!                time_now)
+!        else
+!            call canopy_write_txt((trim(file_out(1)) // '_t' // ADJUSTL(nn_string)), &
+!                time_now)
+!        end if
 
 #ifdef NETCDF
         !only output if 2D input NCF is used
+        call canopy_write_ncf(trim(file_out(1)) // '_' // ADJUSTL(time_now_file))
+
 !        call canopy_write_ncf(trim(file_out(1)))
-        if (nn.lt.10) then
-            call canopy_write_ncf(trim(file_out(1)) // '_t00' // ADJUSTL(nn_string))
-        else if (nn.ge.10.and.nn.lt.100) then
-            call canopy_write_ncf(trim(file_out(1)) // '_t0' // ADJUSTL(nn_string))
-        else
-            call canopy_write_ncf(trim(file_out(1)) // '_t' // ADJUSTL(nn_string))
-        end if
+!        if (nn.lt.10) then
+!            call canopy_write_ncf(trim(file_out(1)) // '_t00' // ADJUSTL(nn_string))
+!        else if (nn.ge.10.and.nn.lt.100) then
+!            call canopy_write_ncf(trim(file_out(1)) // '_t0' // ADJUSTL(nn_string))
+!        else
+!            call canopy_write_ncf(trim(file_out(1)) // '_t' // ADJUSTL(nn_string))
+!        end if
 #endif
 
 ! Update new date and time
